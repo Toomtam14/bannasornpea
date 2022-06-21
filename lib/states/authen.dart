2dc39edffe57fc96dcf23445fdcaf37b10e32fe7
Toomtam <tambannasorn@gmail.com>
2022-06-21 11:24:45 +0700

    form success

--- lib/states/authen.dart
+++ lib/states/authen.dart
@@ -1,4 +1,5 @@
 import 'package:bannasornpea/utility/my_constant.dart';
+import 'package:bannasornpea/widgets/show_form.dart';
 import 'package:bannasornpea/widgets/show_image.dart';
 import 'package:bannasornpea/widgets/show_text.dart';
 import 'package:flutter/material.dart';
@@ -10,24 +11,50 @@ class Authen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints boxConstraints) {
+          builder: (BuildContext context, BoxConstraints boxConstraints) {
         return Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               newLogo(boxConstraints),
-              ShowText(text: 'Login :', textStyle: MyConstant().h1Style(),)
+              newTitle(),
+              formUser(boxConstraints),
+              Container(
+                margin: const EdgeInsets.only(top: 16),
+                height: 40,
+                width: boxConstraints.maxWidth * 0.6,
+                child: ShowForm(
+                  hint: 'Password :',
+                  iconData: Icons.electric_meter,
+                  changeFung: (String string) {},
+                ),
+              )
             ],
           ),
         );
-      }
+      }),
+    );
+  }
+
+  Container formUser(BoxConstraints boxConstraints) {
+    return Container(
+      margin: const EdgeInsets.only(top: 16),
+      width: boxConstraints.maxWidth * 0.6,
+      height: 40,
+      child: ShowForm(
+        hint: 'User :',
+        iconData: Icons.bolt_outlined,
+        changeFung: (String string) {},
       ),
     );
   }
 
+  ShowText newTitle() => ShowText(
+        text: 'Login :',
+        textStyle: MyConstant().h1Style(),
+      );
+
   SizedBox newLogo(BoxConstraints boxConstraints) {
-    return SizedBox(
-        width: boxConstraints.maxWidth*0.25, 
-        child: ShowImage()
-        );
+    return SizedBox(width: boxConstraints.maxWidth * 0.25, child: ShowImage());
   }
 }
