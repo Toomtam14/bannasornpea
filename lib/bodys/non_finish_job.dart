8cb64519f55e0b99580922bb01ee711bf4d57e38
Toomtam <tambannasorn@gmail.com>
2022-06-22 15:40:27 +0700

    finish and non finish

---
+++ lib/bodys/non_finish_job.dart
@@ -0,0 +1,11 @@
+import 'package:bannasornpea/widgets/show_text.dart';
+import 'package:flutter/material.dart';
+
+class NonFinishJob extends StatelessWidget {
+  const NonFinishJob({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ShowText(text: 'This is Non Finish Job');
+  }
+}
--- lib/states/my_service.dart
+++ lib/states/my_service.dart
@@ -1,39 +1,91 @@
+import 'package:bannasornpea/bodys/finish_job.dart';
+import 'package:bannasornpea/bodys/non_finish_job.dart';
 import 'package:bannasornpea/utility/my_constant.dart';
 import 'package:bannasornpea/utility/my_dialog.dart';
 import 'package:bannasornpea/widgets/show_icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class MyService extends StatelessWidget {
+class MyService extends StatefulWidget {
   const MyService({Key? key}) : super(key: key);
+
+  @override
+  State<MyService> createState() => _MyServiceState();
+}
+
+class _MyServiceState extends State<MyService> {
+  var titles = <String>[
+    'Non Finish',
+    'Finish',
+  ];
+  var iconDatas = <IconData>[
+    Icons.close,
+    Icons.done,
+  ];
+  var widgets = <Widget>[
+    const NonFinishJob(),
+    const FinishJob(),
+  ];
+  var bottonNavigators = <BottomNavigationBarItem>[];
+  int indexBodys = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    for (var i = 0; i < titles.length; i++) {
+      bottonNavigators.add(
+        BottomNavigationBarItem(
+          label: titles[i],
+          icon: Icon(
+            iconDatas[i],
+          ),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        foregroundColor: MyConstant.dark,
-        backgroundColor: Colors.white,
-        actions: [
-          ShowIconButton(
-            iconData: Icons.exit_to_app,
-            pressFunc: () {
-              MyDialog(context: context).normalDialog(
-                  pressFunc: () async {
-                    SharedPreferences preferences =
-                        await SharedPreferences.getInstance();
-                    preferences.clear().then((value) {});
-                    Navigator.pushNamedAndRemoveUntil(
-                        context, '/authen', (route) => false);
-                  },
-                  label: 'SignOut',
-                  title: 'Sign Out?',
-                  subTitle: 'Please Confirm SignOut');
-            },
-          ),
-        ],
+      appBar: newAppBar(context),
+      body: widgets[indexBodys],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: indexBodys,
+        items: bottonNavigators,
+        onTap: (value){
+          setState(() {
+            indexBodys = value;
+          });
+        },
       ),
+    );
+  }
+
+  AppBar newAppBar(BuildContext context) {
+    return AppBar(
+      elevation: 0,
+      foregroundColor: MyConstant.dark,
+      backgroundColor: Colors.white,
+      actions: [
+        ShowIconButton(
+          iconData: Icons.exit_to_app,
+          pressFunc: () {
+            MyDialog(context: context).normalDialog(
+                pressFunc: () async {
+                  SharedPreferences preferences =
+                      await SharedPreferences.getInstance();
+                  preferences.clear().then((value) {});
+                  Navigator.pushNamedAndRemoveUntil(
+                      context, '/authen', (route) => false);
+                },
+                label: 'SignOut',
+                title: 'Sign Out?',
+                subTitle: 'Please Confirm SignOut');
+          },
+        ),
+      ],
     );
   }
 }
